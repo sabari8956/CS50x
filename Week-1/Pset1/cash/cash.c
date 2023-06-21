19bfc228fa0a01565727ef4afe20f1593ef0f751
Sabari <education@mindmy.tech>
2023-06-21 19:20:35 +0530

    prototypes on cash

---
+++ Week-1/Pset1/cash/cash.c
@@ -0,0 +1,8 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
